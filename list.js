aae5fa86c4bf74f2d5fafd5a3e078e1d95c3ba36
todoom1 <aa13976383@outlook.com>
2026-02-06 02:45:32 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,19 @@
+document.getElementById("jl").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9NjU4NDQmc2VsZlBsYW5JZD01MDcxNA"), '_blank'); });
+document.getElementById("st").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTMwOTg3JnNlbGZQbGFuSWQ9MTE0NzE"), '_blank'); });
+document.getElementById("zt").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMxJnB0PTJCMjA2RTIxLTQ2MUYtRDI3QS1FRUUzLTk0RUY3NThGODQwOA"), '_blank'); });
+document.getElementById("ff").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQ0NjE3NzAzMDgyMTQ3MzEmc2VsZlBsYW5JZD0xNDI0OQ"), '_blank'); });
+document.getElementById("yh").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9Q0E3RkZEREUtNEU1NC0zRDZELThEMEEtNTQ0MzRFRjg2OTQw"), '_blank'); });
+document.getElementById("xsd").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9kZXJlYXIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA1OSZ0b3BJZD0xMTQwNzgmc2VsZlBsYW5JZD02NzA2NDk"), '_blank'); });
+document.getElementById("t1").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUxJnRvcElkPTUyNzQxOQ"), '_blank'); });
+document.getElementById("cf").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTIyOTg0"), '_blank'); });
+
+document.getElementById("jln").innerHTML = ("君临国际");
+document.getElementById("stn").innerHTML = ("胜天国际");
+document.getElementById("ztn").innerHTML = ("征途国际");
+document.getElementById("ffn").innerHTML = ("非凡娱乐");
+document.getElementById("yhn").innerHTML = ("壹号娱乐");
+document.getElementById("xsdn").innerHTML = ("新时代  ");
+document.getElementById("t1n").innerHTML = ("TOP1体育");
+document.getElementById("cfn").innerHTML = ("超凡国际");
+document.getElementById("2").innerHTML = ("十年信誉平台推荐");
+document.getElementById("1").innerHTML = ("KKP9.MY");
