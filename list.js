54c8be88b09d5433bb716a591d8cbc8cfab607e2
todoom1 <aa13976383@outlook.com>
2026-02-09 17:42:17 +0800

    Update list.js

--- list.js
+++ list.js
@@ -16,4 +16,21 @@ document.getElementById("xsdn").innerHTML = ("新时代  ");
 document.getElementById("t1n").innerHTML = ("TOP1体育");
 document.getElementById("cfn").innerHTML = ("超凡国际");
 document.getElementById("2").innerHTML = ("十年信誉平台推荐");
+
 document.getElementById("1").innerHTML = ("KKP9.MY");
+const texts = ["祝您新年快乐！", "马到成功", "KKP9.MY"];
+const today = new Date();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+let currentIndex = 0;
+
+if (month === 2 && date === 17) {
+    alert("🎇祝您新年快乐！🎇"); 
+setInterval(() => {
+document.getElementById("1").innerHTML = texts[currentIndex];
+currentIndex = (currentIndex + 1) % texts.length;
+}, 2026);
+
+} else if (month === 3 && date === 3) {
+    alert("🎇祝您元宵节快乐！🎇");
+}
