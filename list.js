1da0a177106d8b2a0cd1a5fa7880d533a312674b
todoom1 <aa13976383@outlook.com>
2026-03-21 03:13:43 +0800

    更新 list.js

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 document.getElementById("jl").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9NjU4NDQmc2VsZlBsYW5JZD01MDcxNA"), '_blank'); });
 document.getElementById("st").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTMwOTg3JnNlbGZQbGFuSWQ9MTE0NzE"), '_blank'); });
 document.getElementById("zt").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMxJnB0PTJCMjA2RTIxLTQ2MUYtRDI3QS1FRUUzLTk0RUY3NThGODQwOA"), '_blank'); });
-document.getElementById("ff").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQ0NjE3NzAzMDgyMTQ3MzEmc2VsZlBsYW5JZD0xNDI0OQ"), '_blank'); });
+document.getElementById("ff").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTc0NjE3NzQwMDY3MDA3MDEmc2VsZlBsYW5JZD00NTg0NDM4"), '_blank'); });
 document.getElementById("yh").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9Q0E3RkZEREUtNEU1NC0zRDZELThEMEEtNTQ0MzRFRjg2OTQw"), '_blank'); });
 document.getElementById("xsd").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9kZXJlYXIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA1OSZ0b3BJZD0xMTQwNzgmc2VsZlBsYW5JZD02NzA2NDk"), '_blank'); });
 document.getElementById("t1").addEventListener("click", function() {window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUxJnRvcElkPTUyNzQxOQ"), '_blank'); });
